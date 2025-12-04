86c0b494fe60e544ef53711ec44a8bb164e23d2b
Miguel Mano <aquelemiguel@gmail.com>
2025-12-04 17:32:03 +0000

    feat: add day 4

---
+++ d4/d4.ts
@@ -0,0 +1,59 @@
+function isAccessible(grid: string[][], cx: number, cy: number) {
+  if (grid[cx][cy] !== "@") {
+    return false;
+  }
+  const dirs = [
+    [-1, -1], [0, -1], [1, -1],
+    [-1,  0],          [1,  0],
+    [-1,  1], [0,  1], [1,  1],
+  ];
+
+  let adj = 0;
+  for (const [dx, dy] of dirs) {
+    const nx = cx + dx;
+    const ny = cy + dy;
+
+    if (
+      nx >= 0 && nx < grid.length &&
+      ny >= 0 && ny < grid[0].length &&
+      grid[nx][ny] === "@"
+    ) {
+      adj++;
+    }
+  }
+  return adj < 4;
+}
+
+function forklift(grid: string[][], options: {single: boolean}) {
+  let removed = 0;
+
+  while (true) {
+    const pass = [];
+    for (let i = 0; i < grid.length; i++) {
+      for (let j = 0; j < grid[i].length; j++) {
+        if (isAccessible(grid, i, j)) {
+          pass.push([i, j]);
+        }
+      }
+    }
+    removed += pass.length;
+    if (pass.length === 0 || options.single) {
+      return removed;
+    }
+    for (const [x, y] of pass) {
+      grid[x][y] = '.';
+    }
+  }
+}
+
+export function p1(grid: string[][]): number {
+  return forklift(grid, { single: true });
+}
+
+export function p2(grid: string[][]): number {
+  return forklift(grid, { single: false });
+}
+
+export function parseInput(input: string) {
+  return input.split("\n").map((l) => l.split(""));
+}
